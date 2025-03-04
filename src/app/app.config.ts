065379e456840196260c5f7fc0332c211469d550
MiskaCs90 <miskacs90@icloud.com>
2025-03-04 22:02:02 +0100

    Firebase and Angular Material installed

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,29 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { firebaseConfig } from './firebase.config';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideFirebaseApp(() => initializeApp({
+      projectId: firebaseConfig.projectId,
+      appId: firebaseConfig.appId,
+      storageBucket: firebaseConfig.storageBucket,
+      apiKey: firebaseConfig.apiKey,
+      authDomain: firebaseConfig.authDomain,
+      messagingSenderId: firebaseConfig.messagingSenderId,
+      measurementId: firebaseConfig.measurementId
+    })),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase()),
+    provideMessaging(() => getMessaging())]
 };

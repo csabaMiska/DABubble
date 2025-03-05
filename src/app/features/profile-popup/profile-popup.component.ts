192aec9ab6911a0845ad91877f864bda888216f6
Patrick Klein <131147594+PatrickKL1610@users.noreply.github.com>
2025-03-05 15:47:01 +0100

    add profile-popup component

---
+++ src/app/features/profile-popup/profile-popup.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-popup',
+  imports: [],
+  templateUrl: './profile-popup.component.html',
+  styleUrl: './profile-popup.component.scss'
+})
+export class ProfilePopupComponent {
+
+}

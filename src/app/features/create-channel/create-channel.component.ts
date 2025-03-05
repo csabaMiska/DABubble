7bf07822e13f2295a5e17ed25bd1f8c18499bae8
unknown <jessicamanczak@gmx.de>
2025-03-05 12:49:12 +0100

    added create-component

---
+++ src/app/features/create-channel/create-channel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-channel',
+  imports: [],
+  templateUrl: './create-channel.component.html',
+  styleUrl: './create-channel.component.scss'
+})
+export class CreateChannelComponent {
+
+}

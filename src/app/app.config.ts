dcdfa619d41588716a12def3c914523a369d9451
Konstantin Aksenov <kav.pbg@gmail.com>
2025-03-06 16:22:09 -0300

    Füge Lizenzinformationen hinzu und verbessere die Formatierung der Firebase-Konfiguration.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,32 +2,33 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideClientHydration, withEventReplay} from '@angular/platform-browser';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getDatabase, provideDatabase } from '@angular/fire/database';
 import { getMessaging, provideMessaging } from '@angular/fire/messaging';
 import { firebaseConfig } from './firebase.config';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
-    provideFirebaseApp(() => initializeApp({
-      projectId: firebaseConfig.projectId,
-      appId: firebaseConfig.appId,
-      storageBucket: firebaseConfig.storageBucket,
-      apiKey: firebaseConfig.apiKey,
-      authDomain: firebaseConfig.authDomain,
-      messagingSenderId: firebaseConfig.messagingSenderId,
-      measurementId: firebaseConfig.measurementId
-    })),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: firebaseConfig.projectId,
+        appId: firebaseConfig.appId,
+        storageBucket: firebaseConfig.storageBucket,
+        apiKey: firebaseConfig.apiKey,
+        authDomain: firebaseConfig.authDomain,
+        messagingSenderId: firebaseConfig.messagingSenderId,
+        measurementId: firebaseConfig.measurementId,
+      })
+    ),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase()),
     provideMessaging(() => getMessaging()),
-    provideAnimationsAsync()
-  ]
+  ],
 };

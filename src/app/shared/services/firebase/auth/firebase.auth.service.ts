b81efc6e64fab6e5203c7f5dff8711e58692a8ee
MiskaCs90 <miskacs90@icloud.com>
2025-03-28 15:35:12 +0100

    refactor: remove unused client hydration provider and console log in Firebase authentication service

--- src/app/shared/services/firebase/auth/firebase.auth.service.ts
+++ src/app/shared/services/firebase/auth/firebase.auth.service.ts
@@ -57,7 +57,6 @@ export class FirebaseAuthService {
       signInWithEmailAndPassword(this.auth, email, password)
         .then((userCredential) => {
           const user = userCredential.user;
-          console.log(user);
           this.checkemailverification(user);
           observer.next(userCredential);
           observer.complete();
